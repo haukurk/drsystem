afec2f4d6a3c37ce29681f84c5d7cb935006feab
Haukur Kristinsson <haukur@hauxi.is>
2015-04-08 16:48:30 +0000

    Adding initialization of subtypes for ReviewEntityModel for the API.

--- Api/Models/ReviewEntityModel.cs
+++ Api/Models/ReviewEntityModel.cs
@@ -7,16 +7,25 @@ namespace Api.Models
 {
     public class ReviewEntityModel
     {
+
+        public ReviewEntityModel()
+        {
+            Issues = new List<IssueModel>();
+            System = new SystemModel();
+            User = new UserBaseModel();
+        }
+
         public int Id { get; set; }
         public string Description { get; set; }
         public string Responsible { get; set; }
         public string ResponsibleEmail { get; set; }
         public int NotificationPeriod { get; set; }
-        public DateTime LastNotified { get; set; }
+        public DateTime? LastNotified { get; set; }
         public string IdentityString { get; set; }
         public string PermanentUrl { get; set; }
         public SystemModel System { get; set; }
         public List<IssueModel> Issues { get; set; }
+        public UserBaseModel User { get; set; }
 
         public string Url { get; set; }
     }
