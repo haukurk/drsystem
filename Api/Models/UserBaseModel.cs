bfca2f48f91dbca6e6b322b1fb59d2dd8b1e6426
Haukur Kristinsson <haukur@hauxi.is>
2015-04-08 16:47:45 +0000

    Adding a Fullname property to UserBaseModel for API.

--- Api/Models/UserBaseModel.cs
+++ Api/Models/UserBaseModel.cs
@@ -11,7 +11,7 @@ namespace Api.Models
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Fullname { get; set; }
+        public string Fullname { get { return String.Format("{0} {1}", this.FirstName, this.LastName); } }
         public DateTime? RegistrationDate { get; set; }
 
         public string Url { get; set; }
