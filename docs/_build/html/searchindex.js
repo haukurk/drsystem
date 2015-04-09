c1acaf2697d53de8cad92525a3e1934435088fef
Haukur Kristinsson <haukur@hauxi.is>
2015-04-09 11:47:35 +0000

    deleted build folder for Sphinx

--- docs/_build/html/searchindex.js
+++ docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to drsystem&#8217;s documentation!"],objects:{},titleterms:{drsystem:0,document:0,welcom:0,indic:0,tabl:0}})
+Search.setIndex({envversion:42,terms:{index:0,search:0,modul:0,system:0,content:0,section:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to DRS&#8217;s documentation!"],objects:{},titleterms:{welcom:0,specif:0,drsystem:[],configur:0,indic:0,api:0,tabl:0,document:0}})
